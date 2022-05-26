e6fb73f6c6b8db334e8ee14dc429e3a9e2acaac2
Ivan Makarov <makivanik@gmail.com>
2022-05-26 02:03:55 +0200

    add key property to component

--- components/bundleComponents.js
+++ components/bundleComponents.js
@@ -1,5 +1,8 @@
 import { getElement } from "./util.js";
 
 export default (components) => {
-    return getElement('div', null, components)
+    const componentsWithKeys = components.map((component, index) => {
+        return {...component, key: index }
+    })
+    return getElement('div', null, componentsWithKeys)
 }
